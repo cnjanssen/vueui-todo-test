17839b7e9e9abb3a79b13137bdb1aa39c736de9e
Christopher Janssen <christopherjanssen@MacBook-Pro-3.local>
2020-03-10 11:54:25 -0700

    added VUE UI

---
+++ todo-app/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Navigation>
+    </Navigation>
+    <router-view/>
+    <!-- We use kebab-case in our HTML per vue style guides -->
+    <todo-list>
+    </todo-list>
+  </div>
+</template>
+
+<script>
+
+// import our own modules here
+import TodoList from './components/TodoList'
+import Navigation from './components/Navigation'
+export default {
+  name: 'App',
+  components: {
+    // We use camelCase within JavaScript per vue style guides
+    // read more here: https://vuejs.org/v2/style-guide/#Prop-name-casing-strongly-recommended
+    TodoList,
+    Navigation
+  }
+  // data function is put in our main App.vue, NOT the component itself
+  // TODO: adapt data used in TailWindUI to match format given here: https://scotch.io/tutorials/build-a-to-do-app-with-vue-js-2
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
